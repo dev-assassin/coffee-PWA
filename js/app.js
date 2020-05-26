a736b543e342059928b167089376c61b9de4348c
dev-assassin <alexandra.uefa@gmail.com>
2020-05-26 12:46:38 +0300

    add logos and done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,49 @@
 const container = document.querySelector(".container")
-const coffees = [
-    { name: "Perspiciatis", image: "images/coffee1.jpg" },
-    { name: "Voluptatem", image: "images/coffee2.jpg" },
-    { name: "Explicabo", image: "images/coffee3.jpg" },
-    { name: "Rchitecto", image: "images/coffee4.jpg" },
-    { name: " Beatae", image: "images/coffee5.jpg" },
-    { name: " Vitae", image: "images/coffee6.jpg" },
-    { name: "Inventore", image: "images/coffee7.jpg" },
-    { name: "Veritatis", image: "images/coffee8.jpg" },
-    { name: "Accusantium", image: "images/coffee9.jpg" },
+const coffees = [{
+        name: "Perspiciatis",
+        image: "images/coffee1.jpg"
+    },
+    {
+        name: "Voluptatem",
+        image: "images/coffee2.jpg"
+    },
+    {
+        name: "Explicabo",
+        image: "images/coffee3.jpg"
+    },
+    {
+        name: "Rchitecto",
+        image: "images/coffee4.jpg"
+    },
+    {
+        name: " Beatae",
+        image: "images/coffee5.jpg"
+    },
+    {
+        name: " Vitae",
+        image: "images/coffee6.jpg"
+    },
+    {
+        name: "Inventore",
+        image: "images/coffee7.jpg"
+    },
+    {
+        name: "Veritatis",
+        image: "images/coffee8.jpg"
+    },
+    {
+        name: "Accusantium",
+        image: "images/coffee9.jpg"
+    },
 ]
 
 const showCoffees = () => {
     let output = ""
     coffees.forEach(
-      ({ name, image }) =>
+        ({
+            name,
+            image
+        }) =>
         (output += `
                 <div class="card">
                   <img class="card--avatar" src=${image} />
@@ -27,3 +56,12 @@ const showCoffees = () => {
 }
 
 document.addEventListener("DOMContentLoaded", showCoffees)
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
